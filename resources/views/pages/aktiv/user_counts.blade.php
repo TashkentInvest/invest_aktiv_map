ae673fe3b7871c6049b925be692d30a38474cd04
InvestUz <invest.abdusattorov@gmail.com>
2024-10-30 18:13:13 +0500

    fixed filter

--- resources/views/pages/aktiv/user_counts.blade.php
+++ resources/views/pages/aktiv/user_counts.blade.php
@@ -5,7 +5,7 @@
 
     @if ($users->count())
         <div class="table-responsive rounded shadow-sm">
-            <table class="table table-hover table-bordered align-middle">
+            <table id="users-table" class="table table-hover table-bordered align-middle">
                 <thead class="table-primary">
                     <tr>
                         <th scope="col">Фойдаланувчи ID</th>
@@ -18,22 +18,24 @@
                 </thead>
                 <tbody>
                     @foreach ($users as $user)
-                        <tr>
-                            <td>{{ $user->id }}</td>
-                            <td>{{ $user->name }}</td>
-                            <td>{{ $user->email }}</td>
-                            <td>
-                                @foreach ($user->roles as $role)
-                                    <span class="badge bg-info text-dark">{{ $role->name }}</span>
-                                @endforeach
-                            </td>
-                            <td>{{ $user->aktivs_count }}</td>
-                            {{-- <td>
-                                <a href="{{ route('aktivs.index', ['user_id' => $user->id]) }}" class="btn btn-primary btn-sm">
-                                    Активларини кўриш
-                                </a>
-                            </td> --}}
-                        </tr>
+                        @if($user->name != 'Employee')
+                            <tr>
+                                <td>{{ $user->id }}</td>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>
+                                    @foreach ($user->roles as $role)
+                                        <span class="badge bg-info text-dark">{{ $role->name }}</span>
+                                    @endforeach
+                                </td>
+                                <td>{{ $user->aktivs_count }}</td>
+                                {{-- <td>
+                                    <a href="{{ route('aktivs.index', ['user_id' => $user->id]) }}" class="btn btn-primary btn-sm">
+                                        Активларини кўриш
+                                    </a>
+                                </td> --}}
+                            </tr>
+                        @endif
                     @endforeach
                 </tbody>
             </table>
@@ -41,4 +43,23 @@
     @else
         <p>Фойдаланувчилар топилмади.</p>
     @endif
+@endsection
+
+@section('scripts')
+    <!-- Include jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" crossorigin="anonymous"></script>
+    <!-- Include DataTables CSS and JS -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <!-- Initialize DataTables -->
+    <script>
+        $(document).ready(function() {
+            $('#users-table').DataTable({
+                // Optional: Customize DataTables language settings
+                language: {
+                    url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/Uzbek.json' // Adjust the URL if needed
+                }
+            });
+        });
+    </script>
 @endsection
