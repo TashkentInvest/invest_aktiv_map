881e6124c5e91512fcfb7cadebffc674fc79588a
InvestUz <invest.abdusattorov@gmail.com>
2024-11-02 18:38:15 +0500

    test

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -637,10 +637,10 @@
                     </div>
                     <div class="col-sm-6 ms-auto my-1">
                         <ul class="list-inline footer-link mb-0 justify-content-sm-end d-flex">
-                            <li class="list-inline-item"><a href="../index.html">Home</a></li>
+                            {{-- <li class="list-inline-item"><a href="../index.html">Home</a></li>
                             <li class="list-inline-item"><a href="https://pcoded.gitbook.io/light-able/"
-                                    target="_blank">Documentation</a></li>
-                            <li class="list-inline-item"><a href="https://phoenixcoded.support-hub.io/"
+                                    target="_blank">Documentation</a></li> --}}
+                            <li class="list-inline-item"><a href="tel:+998997906699"
                                     target="_blank">Support</a></li>
                         </ul>
                     </div>
