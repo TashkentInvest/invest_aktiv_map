68c478e9f00231aaa18dc7e765ac7637bd7946ca
InvestUz <invest.abdusattorov@gmail.com>
2024-11-28 10:24:40 +0500

    some changes

--- resources/views/pages/aktiv/edit.blade.php
+++ resources/views/pages/aktiv/edit.blade.php
@@ -137,7 +137,7 @@
                 <div class="mb-3">
                     <label for="kadastr_raqami">Кадастр рақами</label>
                     <input class="form-control" type="text" name="kadastr_raqami" id="kadastr_raqami"
-                        value="{{ old('kadastr_raqami', $aktiv->kadastr_raqami) }}" pattern="(\d{2}:\d{2}:\d{2}:\d{2}:\d{2}:\d{4})"
+                        value="{{ old('kadastr_raqami', $aktiv->kadastr_raqami) }}" 
                         title="Format: 11:04:42:01:03:0136" placeholder="11:04:42:01:03:0136">
                     <small id="kadastrHelp" class="form-text text-muted">
                         Please enter the cadastral number in the format: 11:04:42:01:03:0136
@@ -164,19 +164,19 @@
                     toggleKadastrRequired();
                 
                     // Kadastr formatting script
-                    document.getElementById('kadastr_raqami').addEventListener('input', function(e) {
-                        let value = e.target.value.replace(/[^0-9]/g, '');
-                        let formattedValue = '';
+                    // document.getElementById('kadastr_raqami').addEventListener('input', function(e) {
+                    //     let value = e.target.value.replace(/[^0-9]/g, '');
+                    //     let formattedValue = '';
                 
-                        if (value.length > 0) formattedValue += value.substring(0, 2);
-                        if (value.length > 2) formattedValue += ':' + value.substring(2, 4);
-                        if (value.length > 4) formattedValue += ':' + value.substring(4, 6);
-                        if (value.length > 6) formattedValue += ':' + value.substring(6, 8);
-                        if (value.length > 8) formattedValue += ':' + value.substring(8, 10);
-                        if (value.length > 10) formattedValue += ':' + value.substring(10, 14);
+                    //     if (value.length > 0) formattedValue += value.substring(0, 2);
+                    //     if (value.length > 2) formattedValue += ':' + value.substring(2, 4);
+                    //     if (value.length > 4) formattedValue += ':' + value.substring(4, 6);
+                    //     if (value.length > 6) formattedValue += ':' + value.substring(6, 8);
+                    //     if (value.length > 8) formattedValue += ':' + value.substring(8, 10);
+                    //     if (value.length > 10) formattedValue += ':' + value.substring(10, 14);
                 
-                        e.target.value = formattedValue;
-                    });
+                    //     e.target.value = formattedValue;
+                    // });
                 </script>
                 
                 <!-- Include Address Partial -->
